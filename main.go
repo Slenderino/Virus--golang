a67e0cf254c24e7b32d3c57c5d6196d837fa0939
Slenderino <theslenderinogamer6000@gmail.com>
2026-06-01 19:43:24 -0400

    refactoring and preparing for game loop

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // --- Auxiliar Types
 type CardType int
 const(
@@ -54,39 +56,17 @@ type Card interface {
 }
 
 func ColorsMatch(c1, c2 Color) bool {
-	return (c1 == c2 || c1 == Multi || c2 == Multi) && !(c1 == NotColor || c2 == NotColor)  
+	return (c1 == c2 || c1 == Multi || c2 == Multi) && !(c1 == NotColor || c2 == NotColor)
 }
 
 // We define the specific characteriistics of a Medicine in order to be considered a Card
 type Medicine struct {
-	Color Color 
+	Color Color
 }
 func (m Medicine) GetColor() Color { return m.Color }
 func (m Medicine) GetCardType() CardType { return MedicineCardType }
-func (m Medicine) ApplyToJoint(g *Game, j *Joint) OperationResult {
-	if !ColorsMatch(j.Base.Color, m.Color) {
-        if len(j.Added) == 0 || !ColorsMatch(j.Added[0].GetColor(), m.Color) {
-            return Illegal
-        }
-    }
-	if j.State == ImmunisedJointState {return Illegal}
-	if len(j.Added) == 0 {
-		j.Added = append(j.Added, m)
-		j.State = VaccinatedJointState
-		return Success
-	}
-	switch j.Added[0].GetCardType() {
-	case VirusCardType: 
-		g.Discard(j.Added[0])
-		g.Discard(m)
-		j.Added = []ApplicableToOrgan{}
-		j.State = FreeJointState
-	case MedicineCardType:
-		j.Added = append(j.Added, m)
-		j.State = ImmunisedJointState
-	}
-	return Success
-}
+func (m Medicine) MedicineFunc() {}
+func (m Medicine) AddonFunc() {}
 
 
 type Organ struct {
@@ -100,43 +80,28 @@ type Virus struct {
 }
 func (v Virus) GetColor() Color { return v.Color }
 func (v Virus) GetCardType() CardType { return VirusCardType }
-func (v Virus) ApplyToJoint(g *Game, j *Joint) OperationResult {
-	if !ColorsMatch(j.Base.Color, v.Color) {
-        if len(j.Added) == 0 || !ColorsMatch(j.Added[0].GetColor(), v.Color) {
-            return Illegal
-        }
-    }
-	if j.State == ImmunisedJointState {return Illegal}
-	if len(j.Added) == 0 {
-		j.Added = append(j.Added, v)
-		j.State = InfectedJointState
-		return Success
-	}
-	switch j.Added[0].GetCardType() {
-	case VirusCardType: 
-		g.Discard(j.Base)
-		g.Discard(j.Added[0])
-		g.Discard(v)
-		return OrganToDestroy
-	case MedicineCardType:
-		g.Discard(j.Added[0])
-		g.Discard(v)
-		j.Added = []ApplicableToOrgan{}
-		j.State = FreeJointState
-	}
-	return Success
-}
+func (v Virus) VirusFunc() {}
+func (v Virus) AddonFunc() {}
 
 type ApplicableToOrgan interface {
 	Card
 	GetColor() Color
-	ApplyToJoint(g *Game, j *Joint) OperationResult
+	AddonFunc()
 }
 
 type Joint struct {
 	Base Organ
 	Added []ApplicableToOrgan
-	State JointState
+	Owner Player //added to be able to acces player from joint
+}
+func (j Joint) DeriveJointState() JointState {
+	if len(j.Added) == 0 { return FreeJointState}
+	if len(j.Added) == 1 {
+		if j.Added[0].GetCardType() == MedicineCardType { return VaccinatedJointState }
+		return InfectedJointState
+	}
+	// len(j.Added) == 2 
+	return VaccinatedJointState 
 }
 
 type Treatment struct {
@@ -160,7 +125,44 @@ type Game struct {
 func (g *Game) Discard(c Card) {
 	g.DiscardPile = append(g.DiscardPile, c)
 }
+func (g *Game) ApplyAddonToJoint(addon ApplicableToOrgan, joint *Joint) OperationResult {
+	baseMatches := ColorsMatch(addon.GetColor(), joint.Base.Color)
+	pureOrgan := joint.DeriveJointState() == FreeJointState
+	addedMatches := !pureOrgan && ColorsMatch(addon.GetColor(), joint.Added[0].GetColor())
+
+	if !baseMatches && !addedMatches {
+		fmt.Printf("ERR: Addon color %v has been tried to apply to joint color %v", addon.GetColor(), joint.Base.Color)
+		return Illegal
+	}
+	
+	if joint.DeriveJointState() == ImmunisedJointState {
+		fmt.Println("ERR: Tried to apply addon to immunised joint")
+		return Illegal
+	}
+
+	if pureOrgan {
+		joint.Added = []ApplicableToOrgan{addon}	
+	} else {
+		if addon.GetCardType() == MedicineCardType {
+			if joint.DeriveJointState() == InfectedJointState {
+				joint.Added = []ApplicableToOrgan{}
+			} else { //VaccinatedJointState
+				joint.Added = append(joint.Added, addon)
+			}
+		} else {
+			if joint.DeriveJointState() == VaccinatedJointState {
+				joint.Added = []ApplicableToOrgan{}
+			} else { //InfectedJointState
+				// remove joint
+				return OrganToDestroy
+			}
+		}
+	}
+	return Success
+}
+
+
 
 func main() {
-	
+
 }
